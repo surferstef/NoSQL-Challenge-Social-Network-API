389aec65171462c48c0c32322ec4af4f32f3312d
stefan hall <stefhall@comcast.net>
2021-07-25 19:50:26 -0700

    Updating user.js from data.js model, updating index.js under models, and adding thought model initialized file

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmmed: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, 'Please enter a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+    },
+    thoughts: [],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Friend'
+        }
+    ]
+
+});
+
+// Add virtual to add more info to database response
+// get total length of user's friends array field
+userSchema.virtual('friendCount').get(() => {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
