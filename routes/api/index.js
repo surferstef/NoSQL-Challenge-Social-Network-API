b470b6a5fd62040abe513f037afad5efb7e9a61b
stefan hall <stefhall@comcast.net>
2021-07-25 21:12:58 -0700

    cleaning up serverJS and routes. Important to note, I will have to update the thought controller to add necessary id logic

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
+const thoughtRoutes = require('./thoughts');
 
 //add prefix of '/users' to routes created in 'users.js'
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//app.use(require('./routes'));
+app.use(require('./routes'));
 
 mongoose.connect(process.envMONGODB_URI || 'mongodb://localhost/network-api', {
     useFindAndModify: false,
