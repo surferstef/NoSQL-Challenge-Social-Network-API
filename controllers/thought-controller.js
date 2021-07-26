68c99d97679ffddf2fbf7fe35ba3df5bbedc3026
stefan hall <stefhall@comcast.net>
2021-07-25 22:32:06 -0700

    Added all updates from 18.3 module to the thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,7 +5,14 @@ const thoughtController = {
     // get all users
     getAllThoughts(req, res) {
         Thought.find({})
-            .then(dbthoughtData => res.json(dbthoughtData))
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('-__v')
+        // sort in DESC order by the _id value
+        .sort({_id: -1 })
+        .then(dbthoughtData => res.json(dbthoughtData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
@@ -15,6 +22,11 @@ const thoughtController = {
     // get one thought by id
     getThoughtById({ params }, res) {
         Thought.findOne({ _id: params.id })
+        .populate({
+            path: 'user',
+            select: '-__v'
+        })
+        .select('__v')
         .then(dbthoughtData => {
             //If no thought found, send 404 error
             if (!dbthoughtData) {
@@ -30,13 +42,107 @@ const thoughtController = {
     },
 
     // create thought
-    createThought({ body }, res) {
+    createThought({ params, body }, res) {
         Thought.create(body)
-        .then(dbthoughtData => res.json(dbthoughtData))
-        .catch(err => res.status(400).json(err));
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { username: body.username },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbthoughtData => {
+            //If no thought found, send 404 error
+            if (!dbthoughtData) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbthoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
     },
 
-    
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        )
+        .then(dbUpdatedThought => {
+            //If no thought found, send 404 error
+            if (!dbUpdatedThought) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbUpdatedThought);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+
+    deleteThought({ params, body }, res) {
+        Thought.findOneAndDelete(
+            { _id: params.id }
+        )
+        .then(dbDeletedThought => {
+            //If no thought found, send 404 error
+            if (!dbDeletedThought) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbDeletedThought);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+
+    createReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true }
+        )  
+        .then(dbthoughtData => {
+        //If no thought found, send 404 error
+        if (!dbthoughtData) {
+            res.status(404).json({ message: 'No thought found with this id'});
+            return;
+        }
+        res.json(dbthoughtData);
+        })
+       .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      })
+    },
+
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: { reactionId: params.reactionId} } },
+            { new: true }
+        )
+        .then(dbthoughtData => {
+            //If no thought found, send 404 error
+            if (!dbthoughtData) {
+                res.status(404).json({ message: 'No thought found with this id'});
+                return;
+            }
+            res.json(dbthoughtData);
+            })
+           .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          })
+    }
+
 
 };
 
